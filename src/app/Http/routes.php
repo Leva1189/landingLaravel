7de6d4cc56ea8e8da8e4ac4d3e73bd53a2ab4c48
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-04-06 14:31:28 +0300

    routerProject

--- src/app/Http/routes.php
+++ src/app/Http/routes.php
@@ -11,6 +11,47 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'web'], function (){
+    Route::match(['get', 'post'], '/', ['uses'=>'IndexController@execute', 'as'=>'home']);
+    Route::get('/page/{alias}', ['uses'=>'PageController@execute', 'as'=>'page']);
+
+    Route::auth();
+});
+
+//admin/page
+Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function (){
+
+    //admin
+    Route::get('/', function (){
+
+    });
+    //admin/pages
+    Route::group(['prefix'=>'pages'], function (){
+        //admin/pages
+        Route::get('/', ['uses'=>'PagesController@execute', 'as'=>'pages']);
+        //admin/pages/add
+        Route::match(['get', 'post'], '/add', ['uses'=>'PagesAddController@execute', 'as'=>'pagesAdd']);
+        //admin/edit/2
+        Route::match(['get', 'post', 'delete'], '/edit/{page}', ['uses'=>'PagesEditController@execute', 'as'=>'pagesEdit']);
+    });
+
+    //admin/portfolios
+    Route::group(['prefix'=>'portfolios'], function (){
+        //admin/portfolios
+        Route::get('/', ['uses'=>'PortfolioController@execute', 'as'=>'portfolio']);
+        //admin/portfolios/add
+        Route::match(['get', 'post'], '/add', ['uses'=>'PortfolioAddController@execute', 'as'=>'portfolioAdd']);
+        //admin/edit/2
+        Route::match(['get', 'post', 'delete'], '/edit/{portfolio}', ['uses'=>'PortfolioEditController@execute', 'as'=>'portfolioEdit']);
+    });
+
+    //admin/services
+    Route::group(['prefix'=>'services'], function (){
+        //admin/services
+        Route::get('/', ['uses'=>'ServiceController@execute', 'as'=>'service']);
+        //admin/services/add
+        Route::match(['get', 'post'], '/add', ['uses'=>'ServiceAddController@execute', 'as'=>'serviceAdd']);
+        //admin/edit/2
+        Route::match(['get', 'post', 'delete'], '/edit/{service}', ['uses'=>'ServiceEditController@execute', 'as'=>'serviceEdit']);
+    });
 });

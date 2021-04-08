18592fb2b0beaa09bebdddfa4e28221cfd361641
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-04-08 10:44:11 +0300

    add maket-layouts-site

---
+++ src/app/Http/Controllers/IndexController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
+
+
+class IndexController extends Controller
+{
+    //
+    public function execute (Request $request) {
+
+        $pages = Page::all();
+        $portfolio = Portfolio::get(array('name', 'filter'));
+        $services = Service::where('id', '<', 20)->get();
+        $peoples = People::take(3)->get();
+
+
+
+
+        return view ('site.index');
+    }
+}

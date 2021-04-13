e70eee20cf399a9b1672557d30a2fb58ba5f09df
Levchenko Dmitry <levchenkodm13@gmail.com>
2021-04-13 10:29:38 +0300

    modul edid

--- src/app/Http/Controllers/PagesEditController.php
+++ src/app/Http/Controllers/PagesEditController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Page;
+use Illuminate\Support\Facades\Validator;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -12,6 +14,47 @@ class PagesEditController extends Controller
     //
     public function execute(Page $page, Request $request)
     {
+        if ($request->isMethod('post')){
+
+            $input = $request->except('_token');
+
+            $validator = Validator::make($input, [
+                'name'=>'required|max:255',
+                'alias'=>'required|max:255|unique:pages,alias,'.$input['id'],
+                'text'=>'required'
+            ]);
+
+            if ($validator->fails()){
+                return redirect()
+                    ->route('pagesEdit', ['page'=>$input['id']])
+                    ->withErrors($validator);
+            }
+
+            //download file
+            if ($request->hasFile('images')){
+                $file = $request->file('images');
+
+                //download file in Direct
+                //getClientOriginalName() - name file
+                $file->move(public_path().'/assets/img',$file->getClientOriginalName());
+
+                //read to db
+                $input['images']=$file->getClientOriginalName();
+            }
+            else{
+                $input['images'] = $input['old_images'];
+            }
+
+            unset($input['old_images']);
+
+            $page->fill($input);
+
+            if ($page->update()){
+                return redirect('admin')->with('status', 'Страница обновлена');
+            }
+
+        }
+
         $old = $page->toArray();
 
         if (view()->exists('admin.pages_edit')){
